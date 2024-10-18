f4e8a2809d83e46c7b82d6686a032861deb13c71
Vida36265 <vm36265@student.uni-lj.si>
2024-10-18 16:03:58 +0100

    domača nalogahrkjbe-rojae-ruhWR

--- Domace_naloge/DN1.ml
+++ Domace_naloge/DN1.ml
@@ -29,7 +29,18 @@ let filter_mapi f sez =
     | prvi::tail ->[f i prvi] @ pom f tail (i+1) in
   List.filter_map (fun x -> x) (pom f sez 0)
 
-  
+
+(* IZOMORFIZMI_______________________________________________________________________________ *)
+let phi1 (a,b) = (b,a)  
+let psi1 = phi1
+
+let phi3 (a, (b,c))= ((a, b), c) 
+let phi3 ((a, b), c) = (a, (b,c))
+
+let phi7 f = (fun x -> fst (f x), fun x -> snd (f x))
+let psi7 (f, g) = fun x -> (f x, g x)
+
+(* POLINOMI__________________________________________________________________________________ *)
 
 type polinom = int list
 
@@ -62,5 +73,76 @@ let ( +++ ) (p:polinom) (q:polinom) =
 let rec ( *** ) p q =
   match p with
   |[] -> []
-  |prvi::tail -> (List.map (fun x -> prvi*x) q) +++ ([0] @ (tail *** q))
+  |prvi::tail -> pocisti (List.map (fun x -> prvi*x) q) +++ ([0] @ (tail *** q))
+
+let rec vrednost (sez:polinom) a = 
+  match sez with
+  | [] -> 0
+  | prvi::tail -> prvi + a * vrednost tail a
+
+let odvod (sez: polinom) =
+  let rec seznamaj sez i =
+    match sez with
+    | [] -> []
+    |x when (List.length x) = i -> [i]
+    | prvi::tail -> [i] @ (seznamaj tail (i+1)) in 
+  match sez with
+  | [] -> []
+  | prvi::tail -> pocisti (List.map2 ( * ) tail (seznamaj sez 1))
+
+
+
+
+let rec izpis (sez:polinom) =
+
+  let rec seznamaj sez i =
+    match sez with
+    | [] -> []
+    (* |x when (List.length x) +2 = i -> [] *)
+      (* ["x^"^ (string_of_int i)] *)
+    | prvi::tail -> 
+      match i with
+      | 0 -> [""] @ (seznamaj tail (i+1))
+      | 1 -> ["x"] @ (seznamaj tail (i+1))
+      | _ -> ["x^" ^ (string_of_int i)] @ (seznamaj tail (i+1))
+    in
+
+  let funkcioniraj a xi =
+    match a with
+    |0 -> ""
+    | 1 -> 
+      (match xi with
+      | "" ->  " + 1"
+      | _ -> " + "  ^  xi)
+    | -1 ->
+      (match xi with
+      | "" ->  " - 1"
+      | _ -> " - "  ^  xi)
+    |x when x < 0 -> " - "  ^ (string_of_int (abs a))^ xi
+    |x when x > 0 -> " + "  ^ (string_of_int (abs a))^ xi
+    |_ -> ""
+    
+  in
+
+  let funkcija_prvih a = 
+    let xi = (List.hd (List.rev (seznamaj sez 0))) in
+    match a with
+    |0 -> ""
+    | 1 -> xi
+    | -1 -> "-"  ^  xi
+    |x when x < 0 -> "-"  ^ (string_of_int a)^ xi
+    |x when x > 0 ->(string_of_int a)^ xi 
+    |_ -> ""
+  in
+
+  let skoraj_koncni_list = List.rev (List.map2 funkcioniraj sez (seznamaj sez 0))
+  in
+  match skoraj_koncni_list with
+  |[] -> ""
+  |_::tail ->
+    let prvi::_ = List.rev sez in
+    (funkcija_prvih prvi) ^ (String.concat "" tail)
+
+  
+
 
